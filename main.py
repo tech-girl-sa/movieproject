55d16f9b9a5e8174d3a1e7872e31efff8535d1cc
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-02-03 15:50:48 +0100

    add support for custom storage files

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from movie_app import MovieApp
+from storage.storage_csv import StorageCsv
 from storage.storage_json import StorageJson
+import sys
 
 
 def main():
-    storage = StorageJson('data/movies.json')
+    try:
+        file_name = sys.argv[1]
+    except IndexError:
+        file_name = "movies.json"
+    file_path = f"data/{file_name}"
+    if file_name[-5:] == ".json":
+        storage = StorageJson(file_path)
+    elif file_name[-4:] == ".csv":
+        storage = StorageCsv(file_path)
+    else:
+        #default to json storage
+        file_path += ".json"
+        storage = StorageJson(file_path)
     movie_app = MovieApp(storage)
     movie_app.run()
 
